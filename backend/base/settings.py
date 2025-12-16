034a31bbc11485c06478e7108688d053cf778f3a
PrimChim <gpritam922@gmail.com>
2025-12-16 19:38:09 +0545

    everything updated for docker

--- backend/base/settings.py
+++ backend/base/settings.py
@@ -83,10 +83,21 @@ WSGI_APPLICATION = 'base.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": config("POSTGRES_DB"),
+        "USER": config("POSTGRES_USER"),
+        "PASSWORD": config("POSTGRES_PASSWORD"),
+        "HOST": "db",
+        "PORT": 5432,
     }
 }
 
@@ -180,4 +191,4 @@ DJOSER = {
 
 SITE_NAME = "Test Django Next.js"
 
-DOMAIN = 'localhost:3000'
+DOMAIN = 'Event Flow -  EMS'
